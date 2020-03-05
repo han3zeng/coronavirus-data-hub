9519ca1e6fde5712d5c3332469c28c8acfb76c4d
Han <allen0346@gmail.com>
2020-03-05 09:41:42 +0800

    Bug: node path + executation location

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-npm run start && npm run upload
+nodePath=~/.nvm/versions/node/v13.9.0/bin/node
+cd /home/ec2-user/code/coronavirus-data-hub
+$nodePath /home/ec2-user/code/coronavirus-data-hub/src/index.js && $nodePath /home/ec2-user/code/coronavirus-data-hub/src/utils/upload-to-s3.js
