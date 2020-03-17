260bef7f4bbf745d7877ef8cd9017499a693d611
Han <allen0346@gmail.com>
2020-03-17 13:05:45 +0800

    Adjust: countries/locations E to C translation

--- src/utils/once/compile-countries-map.js
+++ src/utils/once/compile-countries-map.js
@@ -34,9 +34,24 @@ const countreisXLStoJSON = () => {
   res.others = '其他';
   res['cruise ship'] = '鑽石公主號';
   res['french guiana'] = '法屬圭亞那';
-  res.martinique = '馬丁尼克（法國海外大區）';
-  res.reunion = '留尼旺（法國海外大區）';
-  res['congo (kinshasa)'] = '剛果';
+  res.martinique = '法屬馬丁尼克島';
+  res.reunion = '法屬留尼旺島';
+  res.mayotte = '法屬馬約特島';
+  res['congo (kinshasa)'] = '民主剛果首都：金夏沙';
+  res['congo (brazzaville)'] = '剛果首都：布拉薩';
+  res.guernsey = '根息島';
+  res['the bahamas'] = '巴哈馬';
+  res.greenland = '格陵蘭島';
+  res.jersey = '澤西島';
+  res.aruba = '阿魯巴';
+  res['puerto rico'] = '波多黎各';
+  res.guam = '關島';
+  res.guadeloupe = '法屬瓜地洛普';
+  // overwrite
+  res['holy see'] = '梵蒂岡（教廷）';
+  res['the holy see'] = '梵蒂岡（教廷）';
+  res.uae = '阿拉伯聯合大公國';
+  res['united arab emirates'] = '阿拉伯聯合大公國';
   fs.writeFile(`${targetDirectory}/${fileName}.${fileExtension}`, JSON.stringify(res, null, 2), (err) => {
     if (err) {
       console.log(`fail to write file: ${fileName}: ${err}`);
